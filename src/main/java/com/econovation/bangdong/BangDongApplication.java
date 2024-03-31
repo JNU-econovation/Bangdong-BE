77edd6fd67a80234948bb766494fe6537a1e84df
coco3x <inferior3x@gmail.com>
2024-04-01 00:58:27 +0900

    style: 탭 제거

--- src/main/java/com/econovation/bangdong/BangDongApplication.java
+++ src/main/java/com/econovation/bangdong/BangDongApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BangDongApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(BangDongApplication.class, args);
 	}
-
 }
