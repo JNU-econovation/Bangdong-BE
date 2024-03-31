a155c4cde261bfe1b417a9b89523bbe39a161f66
coco3x <inferior3x@gmail.com>
2024-03-31 23:50:45 +0900

    init: 프로젝트 초기화 및 의존성 추가

---
+++ src/main/java/com/econovation/bangdong/BangDongApplication.java
@@ -0,0 +1,13 @@
+package com.econovation.bangdong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BangDongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BangDongApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/econovation/bangdong/BangDongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.econovation.bangdong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BangDongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
